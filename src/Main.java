3e33ccd5b8d940459c05b180388ea5aab74d6e0e
DNS <glotov-aleks@mail.ru>
2023-04-20 21:23:55 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -9,6 +10,23 @@ public class Main {
         if (clientOS == 1) {
             System.out.println("установите версию приложения для Android по ссылке");
         } else if (clientOS == 0) {
+            System.out.println("установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("нет сообщений");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Решение задачи №2");
+        int clientDeviceYear = 2015;
+        int clientOS = 1;
+        if (clientOS == 1 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear > 2015) {
+            System.out.println("установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear > 2015) {
             System.out.println("установите версию приложения для iOS по ссылке");
         } else {
             System.out.println("нет сообщений");
