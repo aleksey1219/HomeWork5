1df724212095d913d637e0eab22dd5fa60fb41a5
DNS <glotov-aleks@mail.ru>
2023-04-20 21:08:19 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Решение задачи №1");
+        int clientOS = 5;
+        if (clientOS == 1) {
+            System.out.println("установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("нет сообщений");
+        }
     }
 }
